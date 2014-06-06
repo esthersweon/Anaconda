67e3b8f4f21edc0027ef973c4d32d04d7d3e463f
Esther S Weon <esther.s.weon@gmail.com>
2014-06-06 10:52:31 -0700

    rails format

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anaconda::Application.config.secret_key_base = '4db53cbf07df29e7dad6241faa0f95bcfdc0143e9a221f17492753783611f41b72c611b13d1429894a78a41045acd3e3a9891e6a6be9e30aadc32cf7aad78b21'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Anaconda::Application.config.session_store :cookie_store, key: '_Anaconda_session'
